f4dd78977ac92e7b8d45a7350074139f97b7f3cb
Alex Goodship <Algo@whitneyny.whitney.org>
2016-06-06 14:23:17 -0400

    Initial commit of plot1

---
+++ plot1.R
@@ -0,0 +1,20 @@
+
+powerURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(powerURL, "./householdpower.zip", mode = "wb")
+power <- unz("householdpower.zip", "household_power_consumption.txt")
+powerData <- read.table(power, header = TRUE, sep = ";",
+                        colClasses = c("character","factor","numeric",
+                                       "numeric","numeric","numeric", 
+                                       "numeric", "numeric", "numeric"), 
+                        na.strings = "?")
+powerData$Date = as.Date(powerData$Date, "%d/%m/%Y")
+condensePower <- powerData[powerData$Date == c("2007-02-01","2007-02-02"),]
+
+hist(condensePower$Global_active_power, 
+     main = "Global Active Power",
+     col = "red", 
+     ylab = "Frequency", 
+     xlab = "Global Active Power (in kilowatts)")
+
+dev.copy(png, file="plot1.png")
+dev.off()

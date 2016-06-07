cac415128d14c5a4eefce0fa6ca2369c5a875fee
Alex Goodship <Algo@whitneyny.whitney.org>
2016-06-07 14:14:50 -0400

    Initial commit plot.3

---
+++ plot3.R
@@ -0,0 +1,30 @@
+powerURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(powerURL, "./householdpower.zip", mode = "wb")
+power <- unz("householdpower.zip", "household_power_consumption.txt")
+powerData <- read.table(power, header = TRUE, sep = ";",
+                        colClasses = c("character","factor","numeric",
+                                       "numeric","numeric","numeric", 
+                                       "numeric", "numeric", "numeric"), 
+                        na.strings = "?")
+powerData$Date = as.Date(powerData$Date, "%d/%m/%Y")
+condensePower <- powerData[powerData$Date == c("2007-02-01","2007-02-02"),]
+
+datetime1 <- paste(condensePower$Date, condensePower$Time, sep = ";")
+
+datetime <- strptime(datetime1,"%Y-%m-%d;%H:%M:%S")
+
+plot(datetime, condensePower$Sub_metering_1, 
+     type = "l",
+     main = "",
+     xlab = "",
+     ylab = "Energy sub metering")
+
+lines(datetime, condensePower$Sub_metering_2, type = "l", col = "red")
+lines(datetime, condensePower$Sub_metering_3, type = "l", col = "blue")
+
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lty = c(1,1,1),
+       col=c("black", "red", "blue"))
+
+dev.copy(png, file="plot3.png", width=480, height=480)
+dev.off()
